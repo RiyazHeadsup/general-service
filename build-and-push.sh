b9d8e8e2488f57778fcd696d6f1d5315b343e509
tech-headsup <tech@headsupcorporation.com>
2026-02-24 18:03:31 +0530

    lie

--- build-and-push.sh
+++ build-and-push.sh
@@ -5,7 +5,7 @@ set -e
 
 # Configuration
 IMAGE_NAME="hcorptech/general-service-elevate"
-VERSION="0.0.5"
+VERSION="0.0.6"
 PLATFORM="linux/amd64"
 
 echo "🚀 Building and pushing Docker image: ${IMAGE_NAME}:${VERSION}"
